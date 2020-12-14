1c8d3f401ff09e1692e5140674e77c2ad80fcd5d
Soham Tamboli <sohamtamboli@gmail.com>
2020-12-15 03:23:43 +0530

    Added header layout codebase
    Added dropdown frontend
    Setup scss files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [age, setAge] = useState("");
+
+	const handleChange = (event) => {
+		setAge(event.target.value);
+	};
+	return (
+		<div className='app'>
+			<div className='app__body'>
+				<h1 className='app__title'>Covid-19 Tracker</h1>
+				<div className='app__country--selector'>
+					<FormControl variant='filled'>
+						<Select
+							id='demo-simple-select-filled'
+							className='selector__container'
+							value={age}
+							onChange={handleChange}>
+							<MenuItem value='World'>
+								<em>None</em>
+							</MenuItem>
+							<MenuItem value={10}>Ten</MenuItem>
+							<MenuItem value={20}>Twenty</MenuItem>
+							<MenuItem value={30}>Thirty</MenuItem>
+						</Select>
+					</FormControl>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
